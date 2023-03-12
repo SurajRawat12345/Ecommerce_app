ceb4ec479ccc7f11071c9ccdf3f20396f98ac483
SurajRawat12345 <115080037+SurajRawat12345@users.noreply.github.com>
2023-03-12 15:10:28 +0530

    updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoute.js';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // configure env
 dotenv.config();
@@ -15,19 +16,23 @@ dotenv.config();
 // database config
 connectDB();
 
+// ES module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// res object
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path,join(__dirname,"./client/build")));
 
 // Routes
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
-
-
 
 //rest api
 app.use('*', function(req,res){
